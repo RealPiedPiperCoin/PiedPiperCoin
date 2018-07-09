c3709c7cb9f8ccfe7e9dbcffd96836f80f9eebd6
RealPiedPiperCoin <39110914+RealPiedPiperCoin@users.noreply.github.com>
2018-07-09 02:30:13 -0400

    Hail Satan
    Hail Satan > Hello World

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by PiedPiper on 7/9/18.
+//  Copyright © 2018 PiedPiper. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    //
+    std::cout << "Hail Satan!\n";
+    return 0;
+}
